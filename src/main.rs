cfd6d84d4101ff702679c87312d0e38c8515c9c9
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-06 13:41:24 -0500

    progress on calculate_entropy()...

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use std::fs;
 use std::io;
+use std::collections::BTreeMap;
+
+// def scan_memory_dump(file_path: str, device: str, candidates: list = [], chunk_size: int = 32, stride: int = 8):
+/*fn scan_memory_dump(file_path: String, device: String)
+{
+	// TODO: Implement scanning function!!!
+}*/
+
+fn calculate_entropy(bytes: Vec<u8>) //-> u8
+{
+	let mut counts = BTreeMap::new();
+	for i in 0..=255 {
+		counts.insert(i, 0);
+	}
+	
+	for (value, count) in counts.iter() {
+		println!("Value {} count: {}", value, count);
+	}
+	
+	for byte in bytes {
+		//print!("{:02X} ", byte);
+		if let Some(count) = counts.get_mut(&byte) {
+			*count += 1;
+		}
+	}
+	
+	for (value, count) in counts.iter() {
+		println!("Value {} count: {}", value, count);
+	}
+}
 
 fn main() -> io::Result<()> {
 	let file_path = "dump.bin";
@@ -14,6 +44,8 @@ fn main() -> io::Result<()> {
 		print!("{:02X} ", byte);
 	}
 	println!();
+	
+	calculate_entropy(bytes);
 
 	Ok(())
 }
