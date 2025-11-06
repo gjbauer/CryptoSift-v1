68a658dcd409b7e3dd9bb6a2b54a1d0f2be59015
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-06 15:26:10 -0500

    multithreading ✔️

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,62 @@
 use std::fs;
 use std::io;
 use std::collections::BTreeMap;
+use std::sync::Arc;
 use std::thread;
 
-// def scan_memory_dump(file_path: str, device: str, candidates: list = [], chunk_size: int = 32, stride: int = 8):
-/*fn scan_memory_dump(file_path: String, device: String)
+struct PotentialKey
 {
-	// TODO: Implement scanning function!!!
-}*/
+	pos: u64,
+	bytes: Vec<u8>,
+	entropy: f32
+}
 
-fn calculate_entropy(bytes: &Vec<u8>) -> f32
+// def scan_memory_dump(file_path: str, device: str, candidates: list = [], chunk_size: int = 32, stride: int = 8):
+fn scan_memory_dump(bytes: &[u8], chunk_size: Option<usize>, stride: Option<usize>) -> Vec<PotentialKey>
+{
+	let actual_stride = stride.unwrap_or_else(|| 16);
+	let actual_chunk_size = chunk_size.unwrap_or_else(|| 32);
+	// TODO: Implement scanning function!!!
+	let mut keys: Vec<PotentialKey> = Vec::new();
+	
+	for i in (0..=bytes.len()-actual_chunk_size).step_by(actual_stride)
+	{
+		//print!("{:3.2} % into dump...\r", (100 * i / (bytes.len()-actual_chunk_size)));
+		// TODO: Implement message passing to calculate total work by all threads...nice to have, but not our top priority at the moment...
+		
+		// Filter 2: Minimum entropy threshold
+		let entropy = calculate_entropy(&bytes[i..i+actual_chunk_size]);
+		if entropy < 4.65 {
+			continue;
+		}
+	}
+	println!();
+	/*
+	for i in range(0, len(data) - chunk_size, stride):
+		print("{:.2f}".format(100 * (i / (len(data) - chunk_size)))+" % into dump...", end="\r", flush=True)
+		chunk = data[i:i + chunk_size]
+		
+		# Filter 1: Skip known compressed formats
+		if is_known_compressed_format(chunk):
+			continue
+			
+		# Filter 2: Minimum entropy threshold
+		entropy = calculate_entropy(chunk)
+		if entropy < 4.65:
+			continue
+		
+		print("\nPotential key with entropy {:.2f}".format(entropy))
+		candidates.append((i, chunk, entropy))
+		# Sort by entropy + compression ratio (most promising first)
+		candidates.sort(key=lambda x: (x[2]), reverse=True)
+		if len(candidates) > 64:
+			candidates.pop()  # Remove lowest entropy candidate
+	*/
+	
+	keys
+}
+
+fn calculate_entropy(bytes: &[u8]) -> f32
 {
 	let mut counts = BTreeMap::new();
 	for i in 0..=255 {
@@ -30,7 +77,7 @@ fn calculate_entropy(bytes: &Vec<u8>) -> f32
 		}
 	}
 	
-	print!("{:.2} ", entropy);
+	println!("{:.2} ", entropy);
 	
 	return entropy;
 }
@@ -38,7 +85,7 @@ fn calculate_entropy(bytes: &Vec<u8>) -> f32
 fn main() -> io::Result<()> {
 	let file_path = "dump.bin";
 
-	let bytes = fs::read(file_path)?;
+	let bytes = Arc::new(fs::read(file_path)?);
 
 	// Now 'bytes' contains the binary data as a Vec<u8>
 	println!("Read {} bytes from the file.", bytes.len());
@@ -54,9 +101,12 @@ fn main() -> io::Result<()> {
 	let mut children = vec![];
 	
 	for i in 0..=15 {
+		let bytes_clone = Arc::clone(&bytes);
 		// Spin up another thread
 		children.push(thread::spawn(move || {
 		println!("this is thread number {}", i);
+		let slice = &bytes_clone[i..(i+1)*(bytes_clone.len()/16)];
+		scan_memory_dump(slice, None, None);
 		}));
 	}
 
@@ -65,9 +115,9 @@ fn main() -> io::Result<()> {
 		let _ = child.join();
 	}
 	
-	calculate_entropy(&bytes);
+	//calculate_entropy(&bytes);
 	
-	calculate_entropy(&bytes);
+	//calculate_entropy(&bytes);
 
 	Ok(())
 }
