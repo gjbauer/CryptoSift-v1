194de74653042ae220887ffbcbe5aa9c6a11f320
Gabriel Bauer <gabeb1277@gmail.com>
2025-11-07 23:06:23 -0500

    update last status update

--- src/main.rs
+++ src/main.rs
@@ -237,8 +237,7 @@ Usage:
 		let j: usize = queue.iter().map(|s| s.progress).sum();
 		print!("{:3.2} % into dump...\r", (100.0 * j as f32 / bytes.len() as f32));
 	}
-	println!("                    \r");
-	println!("100 % into dump...");
+	println!("100.00 % into dump...");
 	println!("Dump processed!!");
 
 	let mut keys_super = vec![];
